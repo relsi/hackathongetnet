c81c824a68f8f4def55e0c6d6b3efd23778618d0
Rafael <rafael@dietbox.me>
2020-10-11 20:36:40 -0300

    CreditPage

--- lib/app/modules/credit/credit_page.dart
+++ lib/app/modules/credit/credit_page.dart
@@ -18,16 +18,20 @@ class _CreditPageState extends ModularState<CreditPage, CreditController> {
     final colorScheme = Theme.of(context).colorScheme;
     final textTheme = Theme.of(context).textTheme;
     return SafeArea(
-        child: Scaffold(
-      body: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 12.0),
-          child: Column(
+      child: Scaffold(
+        resizeToAvoidBottomPadding: false,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 12.0),
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 SizedBox(height: 15.0),
-                Text("Linhas de Crédito",
-                    style: textTheme.headline5
-                        .copyWith(fontWeight: FontWeight.w600)),
+                Text(
+                  "Linhas de Crédito",
+                  style:
+                      textTheme.headline5.copyWith(fontWeight: FontWeight.w600),
+                ),
                 SizedBox(height: 15.0),
                 Text(
                   "Veja as linhas de crédito que temos disponíveis para o seu empreendimento.",
@@ -36,46 +40,106 @@ class _CreditPageState extends ModularState<CreditPage, CreditController> {
                   ),
                 ),
                 SizedBox(height: 25.0),
-                Text("- Play Cred",
-                    style: textTheme.headline6
-                        .copyWith(fontWeight: FontWeight.w300)),
-                Text(
-                  "Linha de crédito ideal para quem precisa de um aporte para compra de insumos e outros gasto de valores até R\$ 1000,00",
-                  style: textTheme.subtitle1.copyWith(
-                    color: colorScheme.onPrimary.withOpacity(0.5),
+                Card(
+                  color: Colors.red,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      SizedBox(height: 8.0),
+                      const ListTile(
+                        leading: Icon(Icons.person_add),
+                        title: Text(
+                          'PlayCred',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        subtitle: Text(
+                            'Linha de crédito ideal para quem precisa de um aporte para compra de insumos e outros gasto de valores até R\$ 1000,00'),
+                      ),
+                      SizedBox(height: 15.0),
+//                      Row(
+//                        mainAxisAlignment: MainAxisAlignment.end,
+//                        children: <Widget>[
+//                          TextButton(
+//                            child: const Text(
+//                              'Saiba mais',
+//                              style: TextStyle(
+//                                color: Colors.white,
+//                              ),
+//                            ),
+//                            onPressed: () {/* ... */},
+//                          ),
+//                          const SizedBox(width: 8),
+//                        ],
+//                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                Card(
+                  color: Colors.redAccent,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      SizedBox(height: 8.0),
+                      const ListTile(
+                        leading: Icon(Icons.https),
+                        title: Text(
+                          'Crédito Investimento Fixo',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        subtitle: Text(
+                            'Essa é uma linha de crédito pra você que deseja expandir ou modernizar o seu negócio'),
+                      ),
+                      SizedBox(height: 15.0),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                Card(
+                  color: Colors.redAccent,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      SizedBox(height: 8.0),
+                      const ListTile(
+                        leading: Icon(Icons.update),
+                        title: Text(
+                          'Crédito Para Capital de Giro',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        subtitle: Text(
+                            'Ideal para para auxiliar nas suas obrigações de curto prazo'),
+                      ),
+                      SizedBox(height: 15.0),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                Card(
+                  color: Colors.redAccent,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      SizedBox(height: 8.0),
+                      const ListTile(
+                        leading: Icon(Icons.home),
+                        title: Text(
+                          'Home Equity',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                        subtitle: Text(
+                            'Linha de crédito para quem precisa de valores mais elevados e tem imóvel para dar como garantia'),
+                      ),
+                      SizedBox(height: 15.0),
+                    ],
                   ),
                 ),
                 SizedBox(height: 25.0),
-                Text("- Crédito Investimento Fixo",
-                    style: textTheme.headline6
-                        .copyWith(fontWeight: FontWeight.w300)),
-                Text(
-                  "Essa é uma linha de crédito pra você que deseja expandir ou modernizar o seu negócio",
-                  style: textTheme.subtitle1.copyWith(
-                    color: colorScheme.onPrimary.withOpacity(0.5),
-                  ),
-                ),
-                SizedBox(height: 25.0),
-                Text("- Crédito Para Capital de Giro",
-                    style: textTheme.headline6
-                        .copyWith(fontWeight: FontWeight.w300)),
-                Text(
-                  "Ideal para para auxiliar nas suas obrigações de curto prazo",
-                  style: textTheme.subtitle1.copyWith(
-                    color: colorScheme.onPrimary.withOpacity(0.5),
-                  ),
-                ),
-                SizedBox(height: 25.0),
-                Text("- Home Equity",
-                    style: textTheme.headline6
-                        .copyWith(fontWeight: FontWeight.w300)),
-                Text(
-                  "Linha de crédito para quem precisa de valores mais elevados e tem imóvel para dar como garantia",
-                  style: textTheme.subtitle1.copyWith(
-                    color: colorScheme.onPrimary.withOpacity(0.5),
-                  ),
-                ),
-              ])),
-    ));
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
