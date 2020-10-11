ca55503ab7df512404e66fda92abeb157ebf0512
Relsi Maron <relsi@codeup.com.br>
2020-10-11 16:54:54 -0300

    Feature: create the company and credit mockup pages

--- lib/app/modules/business/business_page.dart
+++ lib/app/modules/business/business_page.dart
@@ -10,17 +10,94 @@ class BusinessPage extends StatefulWidget {
   _BusinessPageState createState() => _BusinessPageState();
 }
 
-class _BusinessPageState extends ModularState<BusinessPage, BusinessController> {
+class _BusinessPageState
+    extends ModularState<BusinessPage, BusinessController> {
   //use 'controller' variable to access controller
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text("MEU NEGOCIO"),
-        ),
-      ),
-    );
+    final colorScheme = Theme.of(context).colorScheme;
+    final textTheme = Theme.of(context).textTheme;
+    return SafeArea(
+        child: Scaffold(
+      body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 12.0),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: 15.0),
+                Text("Seu Negócio",
+                    style: textTheme.headline5
+                        .copyWith(fontWeight: FontWeight.w600)),
+                SizedBox(height: 15.0),
+                Text(
+                  "Esses são os dados do seu negócio. Clique no botão Alterar Dados para alterar as informações necessárias",
+                  style: textTheme.subtitle1.copyWith(
+                    color: colorScheme.onPrimary.withOpacity(0.5),
+                  ),
+                ),
+                SizedBox(height: 25.0),
+                Text("Razão Social:",
+                    style: textTheme.headline6
+                        .copyWith(fontWeight: FontWeight.w300)),
+                Text(
+                  "Pedro's Burgueria",
+                  style: textTheme.subtitle1.copyWith(
+                    color: colorScheme.onPrimary.withOpacity(0.5),
+                  ),
+                ),
+                SizedBox(height: 25.0),
+                Text("Endereço:",
+                    style: textTheme.headline6
+                        .copyWith(fontWeight: FontWeight.w300)),
+                Text(
+                  "Rua da República, 602",
+                  style: textTheme.subtitle1.copyWith(
+                    color: colorScheme.onPrimary.withOpacity(0.5),
+                  ),
+                ),
+                SizedBox(height: 25.0),
+                Text("Telefone:",
+                    style: textTheme.headline6
+                        .copyWith(fontWeight: FontWeight.w300)),
+                Text(
+                  "(51) 1234-5678",
+                  style: textTheme.subtitle1.copyWith(
+                    color: colorScheme.onPrimary.withOpacity(0.5),
+                  ),
+                ),
+                SizedBox(height: 25.0),
+                Text("CNPJ:",
+                    style: textTheme.headline6
+                        .copyWith(fontWeight: FontWeight.w300)),
+                Text(
+                  "12.345.678/0001-00",
+                  style: textTheme.subtitle1.copyWith(
+                    color: colorScheme.onPrimary.withOpacity(0.5),
+                  ),
+                ),
+                SizedBox(height: 25.0),
+                Text("E-mail:",
+                    style: textTheme.headline6
+                        .copyWith(fontWeight: FontWeight.w300)),
+                Text(
+                  "pedro@pedrosburguer.com",
+                  style: textTheme.subtitle1.copyWith(
+                    color: colorScheme.onPrimary.withOpacity(0.5),
+                  ),
+                ),
+                SizedBox(height: 35.0),
+                FlatButton(
+                  color: Colors.red,
+                  textColor: Colors.white,
+                  padding: EdgeInsets.all(8.0),
+                  onPressed: () {},
+                  child: Text(
+                    "Alterar Dados",
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                )
+              ])),
+    ));
   }
 }
--- lib/app/modules/credit/credit_page.dart
+++ lib/app/modules/credit/credit_page.dart
@@ -15,12 +15,67 @@ class _CreditPageState extends ModularState<CreditPage, CreditController> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text("LINHA DE CRÉDITO"),
-        ),
-      ),
-    );
+    final colorScheme = Theme.of(context).colorScheme;
+    final textTheme = Theme.of(context).textTheme;
+    return SafeArea(
+        child: Scaffold(
+      body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 12.0),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: 15.0),
+                Text("Linhas de Crédito",
+                    style: textTheme.headline5
+                        .copyWith(fontWeight: FontWeight.w600)),
+                SizedBox(height: 15.0),
+                Text(
+                  "Veja as linhas de crédito que temos disponíveis para o seu empreendimento.",
+                  style: textTheme.subtitle1.copyWith(
+                    color: colorScheme.onPrimary.withOpacity(0.5),
+                  ),
+                ),
+                SizedBox(height: 25.0),
+                Text("- Play Cred",
+                    style: textTheme.headline6
+                        .copyWith(fontWeight: FontWeight.w300)),
+                Text(
+                  "Linha de crédito ideal para quem precisa de um aporte para compra de insumos e outros gasto de valores até R\$ 1000,00",
+                  style: textTheme.subtitle1.copyWith(
+                    color: colorScheme.onPrimary.withOpacity(0.5),
+                  ),
+                ),
+                SizedBox(height: 25.0),
+                Text("- Crédito Investimento Fixo",
+                    style: textTheme.headline6
+                        .copyWith(fontWeight: FontWeight.w300)),
+                Text(
+                  "Essa é uma linha de crédito pra você que deseja expandir ou modernizar o seu negócio",
+                  style: textTheme.subtitle1.copyWith(
+                    color: colorScheme.onPrimary.withOpacity(0.5),
+                  ),
+                ),
+                SizedBox(height: 25.0),
+                Text("- Crédito Para Capital de Giro",
+                    style: textTheme.headline6
+                        .copyWith(fontWeight: FontWeight.w300)),
+                Text(
+                  "Ideal para para auxiliar nas suas obrigações de curto prazo",
+                  style: textTheme.subtitle1.copyWith(
+                    color: colorScheme.onPrimary.withOpacity(0.5),
+                  ),
+                ),
+                SizedBox(height: 25.0),
+                Text("- Home Equity",
+                    style: textTheme.headline6
+                        .copyWith(fontWeight: FontWeight.w300)),
+                Text(
+                  "Linha de crédito para quem precisa de valores mais elevados e tem imóvel para dar como garantia",
+                  style: textTheme.subtitle1.copyWith(
+                    color: colorScheme.onPrimary.withOpacity(0.5),
+                  ),
+                ),
+              ])),
+    ));
   }
 }
